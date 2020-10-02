c5e7af7aa001e267d528d5ee2b5c2def52bee551
Souvik <souvik.9401@gmail.com>
2020-10-02 13:57:43 +0530

    updated the file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 
 app.get('/', function(req,res){
-	res.send("<h2>Hi there, thanks for checking out my work</h2>");
+	res.send("<h2>Hi there, thanks for checking out my work. The server that is responding to you is developed using 'express.js' module in node.js. Please type 'localhost:3000/one' in your URL search bar to view the next page.</h2>");
 });
 
 
